34f882525fa5fe4469c363ce30200003179dd536
lauren <lzmunch@gmail.com>
2018-02-10 16:32:44 -0500

    cry

---
+++ cry/scripts.js
@@ -0,0 +1,11 @@
+function submitUserInfo(form){
+  var userId=form.inputbox1.value;
+  var passwd=form.inputbox2.value;
+  alert("user info submited");
+}
+
+function submitWebInfo(form){
+  var website=form.website.value;
+  var keywords=form.keywords.value;
+  alert("info for website submited");
+}
